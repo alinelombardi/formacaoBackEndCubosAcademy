a2ac46a8bcb6e4acf28684661a71a2c9e2307743
Aline Lombardi <alinelf86@gmail.com>
2023-07-15 18:42:16 -0300

    resolução exercício 2

---
+++ u1-logicaProgramação/exercicios-backend-objetos/02/index.js
@@ -0,0 +1,28 @@
+const carros = [
+    {
+        marca: "Peugeot",
+        modelo: "207 passion",
+        ano: 2010,
+        cor: "Cinza escuro",
+        quantidade_portas: 5,
+        automatico: false
+    },
+    {
+        marca: "Peugeot",
+        modelo: "208",
+        ano: 2015,
+        cor: "Cinza claro",
+        quantidade_portas: 3,
+        automatico: true
+    },
+    {
+        marca: "Peugeot",
+        modelo: "308",
+        ano: 2020,
+        cor: "Prata",
+        quantidade_portas: 5,
+        automatico: true
+    }
+];
+
+console.log(carros);
