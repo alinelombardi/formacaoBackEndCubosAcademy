60928c1c3b18807a36eccc2a13a8e318fa2e0d60
Aline Lombardi <alinelf86@gmail.com>
2023-07-15 18:42:01 -0300

    resolução exercício 1

---
+++ u1-logicaProgramação/exercicios-backend-objetos/01/index.js
@@ -0,0 +1,20 @@
+const endereco = {
+    rua: "Avenida Sabina Baptista Camargo",
+    numero: 2981,
+    complemento: "Bl k apto 24",
+    bairro: "Jardim Novo Ângulo",
+    cep: 13185185,
+    cidade: "Hortolândia",
+    estado: "SP",
+    pais: "Brasil"
+};
+
+const usuario = {
+    nome: "Aline Lombardi Fernandes",
+    email: "alinelf86@gmail.com",
+    telefone: "19982046473",
+    data_nascimento: "05/02/1986",
+    endereco
+};
+
+console.log(usuario);
