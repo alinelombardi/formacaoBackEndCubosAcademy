35829fee3ecb7fd97fa88aa5b2d4d37bd536b62d
Aline Lombardi <alinelf86@gmail.com>
2023-08-13 12:18:05 -0300

    conclusão aula rotas, controladores e intermediários

--- m2-backEnd/primeiroServidor/primeiro-servidor-express/src/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express'); //importei o express
-
-const app = express(); // instanciei o express
-
-app.get('/', (requisicao, resposta) => { // get é um metodo que cria uma rota a barra tem relação com a url. Exemplo: http://localhost:3000/
-    resposta.send('Olá, esse é o meu primeiro servidor com o express')
-});
-
-app.listen(3000); // direciona para a porta 3000
